5ee3ae9e2011beeb1c93a66a0595c5234c359979
Alf <alf.g.jr@gmail.com>
2019-03-02 18:47:19 -0800

    remove old react files

--- old/src/SenderApp.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SenderApp from './SenderApp';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SenderApp />, div);
-});
